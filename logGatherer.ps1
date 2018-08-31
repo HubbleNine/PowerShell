29edd5b138e41c9f458c47a5bee7f8597ca9c1e6
HubbleNine <42842904+HubbleNine@users.noreply.github.com>
2018-08-31 16:29:55 -0500

    Update logGatherer.ps1

--- logGatherer.ps1
+++ logGatherer.ps1
@@ -1,5 +1,6 @@
 $inputfilecsv = "path to your csv"
 $inputdata = Import-Csv $inputfilecsv
+#$HostName = (Get-Wmiobject win32_computersystem).__server
 
 foreach ($name in $inputdata) {
     $HostName = $name.Name
@@ -13,12 +14,16 @@ foreach ($name in $inputdata) {
     Get-EventLog -LogName "application" -EntryType "warning" -After (Get-Date).AddHours(-24) | ConvertTo-HTML | Out-File $File4
     Send-MailMessage -Attachments $File1,$File2,$File3,$File4 -SmtpServer "yourmail-server" -From "$HostName@yourdomain" -To "reciver@yourdomain" -Subject "$Hostname Daily Eventlog-Report"
 }
-<#Modified to loop through csv with a list of servers to be run from a central task server
-The computers are put into a CSV file in the format:
+<#
+Modified to loop through csv with a list of servers to be run from a central task server
 
+The computers are put into a CSV file in the format:
 Name,
 somecomputername,
 someothercomputername,
 
-#Found on Spiceworks: https://community.spiceworks.com/topic/443133-need-a-script-to-capture-windows-event-log-in-last-24-hours?utm_source=copy_paste&utm_campaign=growth
+To run locally on one computer, Comment out $inputfilecsv, $inputdata, the foreach line, the $HostName = $name.Name line, and the last bracket. Then uncomment
+the $HostName = (Get-Wmiobject...)._server line.
+
+Found on Spiceworks: https://community.spiceworks.com/topic/443133-need-a-script-to-capture-windows-event-log-in-last-24-hours?utm_source=copy_paste&utm_campaign=growth
 #>
